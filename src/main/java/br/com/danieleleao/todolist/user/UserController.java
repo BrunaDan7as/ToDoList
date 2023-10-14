472764840fd77a387c9a60aa9a969db6d75b4d82
brunadan7as <b.dantassoares@gmail.com>
2023-10-14 12:49:31 -0300

    Sistema de hash na senha aplicado

--- src/main/java/br/com/danieleleao/todolist/user/UserController.java
+++ src/main/java/br/com/danieleleao/todolist/user/UserController.java
@@ -1,9 +1,14 @@
 package br.com.danieleleao.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /*
  * Modificador
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
     /*
      * String
      * integer
@@ -24,7 +31,19 @@ public class UserController {
      * void
     */
     @PostMapping("/")
-    public void Create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity Create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        // Validação de usuário e criação de mensagem de erro HTTP
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        // Criptografar a senha do usuário com bCrypt
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
