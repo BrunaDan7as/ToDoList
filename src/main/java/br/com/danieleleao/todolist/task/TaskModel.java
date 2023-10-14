2127111aa96effd3b8bde8e60290a9f7df56b857
brunadan7as <b.dantassoares@gmail.com>
2023-10-14 17:23:29 -0300

    Validação de rota feita

---
+++ src/main/java/br/com/danieleleao/todolist/task/TaskModel.java
@@ -0,0 +1,42 @@
+package br.com.danieleleao.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/*
+     * ID
+     * Usuário (id_usuario)
+     * Descrição
+     * Título
+     * Data de Inicio
+     * Data termino
+     * prioridade
+     */
+    @Data
+    @Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    
+
+}
